6a2739f36bf5a438696e8f9342019131a30237d4
Chris Campbell <chris.campbell@dutchie.com>
2024-11-09 19:24:47 -0700

    print request details

--- apps/bar/main.go
+++ apps/bar/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sort"
 )
 
 func main() {
@@ -10,5 +11,27 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from bar!")
+	content := "Hello from bar! Here are the request details:\n\n"
+	content += fmt.Sprintf("Host: %s\n", r.Host)
+	content += fmt.Sprintf("Path: %s\n", r.URL.Path)
+	content += fmt.Sprintf("Method: %s\n", r.Method)
+	content += fmt.Sprintf("Protocol: %s\n", r.Proto)
+	content += fmt.Sprintf("RemoteAddr: %s\n", r.RemoteAddr)
+	content += fmt.Sprintf("RequestURI: %s\n", r.RequestURI)
+
+	content += "Headers:\n"
+	for _, k := range sortedHeaderKeys(r.Header) {
+		content += fmt.Sprintf("  %s: %s\n", k, r.Header.Get(k))
+	}
+
+	fmt.Fprintln(w, content)
+}
+
+func sortedHeaderKeys(headers http.Header) []string {
+	keys := make([]string, 0, len(headers))
+	for k := range headers {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
 }
--- apps/baz/main.go
+++ apps/baz/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sort"
 
 	"math/rand"
 )
@@ -19,6 +20,7 @@ func flakeWrapper(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if rand.Intn(100) < flakeProbability {
 			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintln(w, "Flaked!")
 			return
 		}
 		next.ServeHTTP(w, r)
@@ -26,5 +28,27 @@ func flakeWrapper(next http.Handler) http.Handler {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from baz!")
+	content := "Hello from baz! Here are the request details:\n\n"
+	content += fmt.Sprintf("Host: %s\n", r.Host)
+	content += fmt.Sprintf("Path: %s\n", r.URL.Path)
+	content += fmt.Sprintf("Method: %s\n", r.Method)
+	content += fmt.Sprintf("Protocol: %s\n", r.Proto)
+	content += fmt.Sprintf("RemoteAddr: %s\n", r.RemoteAddr)
+	content += fmt.Sprintf("RequestURI: %s\n", r.RequestURI)
+
+	content += "Headers:\n"
+	for _, k := range sortedHeaderKeys(r.Header) {
+		content += fmt.Sprintf("  %s: %s\n", k, r.Header.Get(k))
+	}
+
+	fmt.Fprintln(w, content)
+}
+
+func sortedHeaderKeys(headers http.Header) []string {
+	keys := make([]string, 0, len(headers))
+	for k := range headers {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
 }
